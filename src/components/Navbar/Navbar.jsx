37b6e0cf85484c0f6cae79bc0ce9f6b4dda67345
Ebadur Rahman <ebadurshohag@gmail.com>
2025-08-19 23:31:25 +0600

    implement Navbar component with navigation links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router";
 
 function Navbar() {
     return (
-        <div>Navbar</div>
-    )
+        <>
+            <header className="bg-[#1E2939] flex justify-between items-center px-14 py-5">
+                {/* logo */}
+                <div>
+                    <NavLink className={"font-bold text-2xl text-[#51A2FF]"} to="/">Taskhub</NavLink>
+                </div>
+                {/* links */}
+                <div className="flex items-center gap-14 text-white">
+                    <NavLink to={"/"}>Home</NavLink>
+                    <NavLink to={"/add-task"}>Add Task</NavLink>
+                    <NavLink to={"/browse-task"}>Browse Task</NavLink>
+                    <NavLink to={"/my-posted-task"}>My Posted Tasks</NavLink>
+                    <NavLink to={"/login"}>Login</NavLink>
+                    <NavLink to={"/register"} className={"bg-[#1447E6] px-4 py-2 rounded-md text-base"}>Sign Up</NavLink>
+                </div>
+            </header>
+        </>
+    );
 }
 
-export default Navbar
+export default Navbar;
